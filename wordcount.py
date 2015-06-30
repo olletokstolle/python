54df4a5b8e666adebb033f4b6d2369f126ea2ab2
Olle S <o.serrander@gmail.com>
2015-06-30 18:08:38 +0200

    commit!

---
+++ wordcount.py
@@ -0,0 +1,100 @@
+import codecs
+import sqlite3
+import string
+import os
+
+"""
+Counts the occurence of words in a text file and saves the result in a database.
+"""
+
+def init(db_name):
+
+    #Connects to the database and creates a table if needed.
+
+    if os.path.isfile(db_name):
+        create = False
+    else:
+        create = True
+
+    global db
+    db = sqlite3.connect(db_name)
+
+    global cursor
+    cursor = db.cursor()
+    
+    if create:
+        cursor.execute('''CREATE TABLE words(word TEXT, count INT) ''')
+        db.commit()
+
+
+def strip_punctuation(filename):
+
+    #Strips punctuation from the input file.
+
+    remove = dict.fromkeys(map(ord, string.punctuation))
+
+    with codecs.open(filename,"r+", "utf-8") as file:
+        rawtext = file.read()
+        output = rawtext.translate(str.maketrans("","", string.punctuation))
+        file.seek(0)
+        file.write(output)
+        file.truncate()
+            
+
+def count_words(filename):
+
+    #Reads the input file and does the counting.
+
+    with codecs.open(filename,"r+", "utf-8") as file:
+
+        wordcount = {}
+
+        for word in file.read().split():
+            word = word.lower()
+            if word not in wordcount:
+                wordcount[word] = 1
+            else:
+                wordcount[word] += 1
+
+    for k,v in wordcount.items():
+        cursor.execute('''INSERT INTO words(word, count) VALUES(?, ?)''', (k.lower(), v))
+        db.commit()
+
+    
+
+def get_word_occurence(word):
+
+    #Returns the occurence of a specific word in the database. If fed a string with more than one word it will count all given words separately.
+
+    word = word.lower()
+
+    if len(word.split(" ")) == 1:
+        
+        cursor.execute('''SELECT * FROM words WHERE word = ?''', (word,))
+        results = cursor.fetchone()
+    
+        if results != None:
+            print("\n Your chosen word '" + results[0] + "' is encountered " + str(results[1]) + " times in the text.")
+        else:
+            print("\n The word '" + word + "' is not found.")
+
+    elif len(word.split(" ")) > 1:
+
+        for w in word.split(" "):
+            cursor.execute('''SELECT * FROM words WHERE word = ?''', (w,))
+            results = cursor.fetchone()
+    
+            if results != None:
+                print("\n Your chosen word '" + results[0] + "' is encountered " + str(results[1]) + " times in the text.")
+            else:
+                print("\n The word '" + w + "' is not found in the text.")
+
+def close_db():
+
+    #Umm... yeah.
+
+    db.close()
+        
+
+if __name__ == "__main__":
+    pass
