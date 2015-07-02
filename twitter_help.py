5484dbd0fbd5207555df329b6005f517516a3787
Olle S <o.serrander@gmail.com>
2015-07-02 23:13:51 +0200

    added a queue-function.

--- twitter_help.py
+++ twitter_help.py
@@ -1,5 +1,8 @@
 import tweepy
 import codecs
+import os
+import random
+import time
 
 """
 Simple stuff for making it easier to play around with twitter.
@@ -20,6 +23,7 @@ def auth():
     global api
     api = tweepy.API(auth, wait_on_rate_limit = True)
 
+
 def collect_tweets(user):
 
     #Collects tweets from specific user, txtfile output.
@@ -31,11 +35,61 @@ def collect_tweets(user):
         for tweet in tweets:
             output.write(tweet.text+"\n")
 
+
 def publish_tweet(string):
 
     #For actual tweeting!
 
-    api.update_status(status=string)
+    if len(string) <= 140:
+        api.update_status(status=string)
+    else:
+        print("Could not tweet, string contained more than 140 chars.")
+        print("error tweet: "+string)
+
+
+def tweet_queue(txtfile, hour_interval):
+
+    #Publishes tweets randomly picked from a textfile. Variables: txtfile (string), hour_interval (int).
+
+    if os.path.isfile(txtfile) == False:
+        print("File "+txtfile+" doesn't exist.")
+        ok = False
+    elif os.stat(txtfile).st_size == 0:
+        print("File "+txtfile+" is empty.")
+        ok = False
+    else:
+        ok = True
+
+    while ok:
+
+        with codecs.open(txtfile, "r+") as queue:
+
+            lines = queue.readlines()
+            tweet = random.choice(lines).replace("\n","")
+            publish_tweet(tweet)
+
+        #This part below removes the used tweet from the txtfile.
+
+        with codecs.open(txtfile, "w+") as queue:
+
+            queue.seek(0)
+
+            if len(lines) > 1:
+
+                for line in lines:
+                    if line[-1:] == "\n":
+                        if line != tweet+"\n":
+                            queue.write(line)
+                    else:
+                        if line != tweet:
+                            queue.write(line)
+            else:
+                queue.write("")
+
+
+        time.sleep((60*60)*hour_interval)
+
+
 
 
 if __name__ == '__main__':
