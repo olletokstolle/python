cdc9d5216ef18cfd6a4ae59429549abdc5cf6af7
Olle S <o.serrander@gmail.com>
2015-07-04 00:51:01 +0200

    test commit

---
+++ git_stuff.py
@@ -0,0 +1,21 @@
+import subprocess
+import time
+
+def git_combo(filename, commit_message):
+
+    user = ""
+    pwd = ""
+
+
+    subprocess.call('git add '+filename, shell=True)
+    time.sleep(5)
+    subprocess.call('git commit -m "'+commit_message+'"', shell=True)
+    time.sleep(5)
+    subprocess.call("git push -u origin master")
+    time.sleep(5)
+
+
+if __name__ == '__main__':
+    git_combo("git_stuff.py", "test commit")
+
+
