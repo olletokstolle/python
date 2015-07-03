e3bb817ce07d84287863820e869afca948e06c06
Olle S <o.serrander@gmail.com>
2015-07-04 01:09:41 +0200

    test

--- git_stuff.py
+++ git_stuff.py
@@ -1,21 +1,24 @@
 import subprocess
 import time
+import argparse
 
 def git_combo(filename, commit_message):
 
-    user = ""
-    pwd = ""
-
+    #Lazy stuff.
 
     subprocess.call('git add '+filename, shell=True)
-    time.sleep(5)
     subprocess.call('git commit -m "'+commit_message+'"', shell=True)
-    time.sleep(5)
     subprocess.call("git push -u origin master", shell=True)
-    time.sleep(5)
 
 
 if __name__ == '__main__':
-    git_combo("git_stuff.py", "test commit")
+
+    parser = argparse.ArgumentParser(description='Git add-commit-push combo script.')
+    parser.add_argument("file", help="file name.", type=str)
+    parser.add_argument("commit", help="commit message", type=str)
+
+    args = parser.parse_args()
+
+    git_combo(args.file, args.commit)
 
 
