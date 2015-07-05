e1491de4615c75396c4e51da9a71823d1c538d61
Olle S <o.serrander@gmail.com>
2015-07-05 23:01:40 +0200

    commit

---
+++ update_readme.py
@@ -0,0 +1,32 @@
+import argparse
+
+"""
+Updates the table in my README.md.
+
+Usage: python update_readme.py "filename" "description"
+
+"""
+
+def update_readme(filename, desc):
+
+    with open("README.md", "a") as f:
+        output ="| {} | {} |".format(filename,desc)
+        f.write("\n"+output)
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(description='README update')
+    parser.add_argument("filename", help="filename", type=str)
+    parser.add_argument("desc", help="description", type=str)
+
+    args = parser.parse_args()
+
+    update_readme(args.filename, args.desc)
+
+
+
+
+
+
+
