50ad710f4eb92850945a07c69cc32910dc471807
Olle S <o.serrander@gmail.com>
2015-07-08 00:23:09 +0200

    improved readability.

--- imdb_scrape.py
+++ imdb_scrape.py
@@ -22,7 +22,7 @@ def init(db_name):
 
     global cursor
     cursor = db.cursor()
-    
+
     if create:
         cursor.execute('''CREATE TABLE dir(director TEXT, title TEXT, year INT, rating FLOAT, users TEXT) ''')
         db.commit()
@@ -44,28 +44,31 @@ def scrape_director(imdb_id):
 
     for div in data2:
         links = div.findAll('a')
+
         for a in links:
             filmurl = 'http://imdb.com' + a['href']
+
             if filmurl not in filmlist and '_dr_' in filmurl:
                 filmlist.append(filmurl)
                 getfilm = requests.get(filmurl)
                 data3 = getfilm.text
                 soup2 = BeautifulSoup(data3)
+
                 try:
-                
                     titles = soup2.select('#overview-top > h1')
                     titles2 = titles[0].get_text().split(' ', 1)[1].translate(str.maketrans('()','  '))
+
                     title = titles2.split('\n')[0]
                     year = titles2.split('\n')[1].strip()
                     rating = soup2.select('#overview-top > div.star-box.giga-star > div.titlePageSprite.star-box-giga-star')[0].get_text()
                     users = soup2.select('#overview-top > div.star-box.giga-star > div.star-box-details > a')[0].get_text().split(' ')[1].replace(',', '')
+
                     cursor.execute('''INSERT INTO dir(director, title, year, rating, users) VALUES(?, ?, ?, ?, ?)''', (name, title, year, rating, users ))
                     db.commit()
-                    print("Scraped: " + title)
-                    
+
                 except IndexError:
                     pass
-            
+
 
 def close_db():
 
