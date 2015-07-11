c5d82374934106120c2d8daff1b6dfb777cecbe0
Olle S <o.serrander@gmail.com>
2015-07-11 02:24:49 +0200

    initial commit.

---
+++ movie_lookup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+"""
+Movie lookup script.
+
+Usage:
+"""
+
+import argparse
+import omdb
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(description='Movie lookup script.')
+    parser.add_argument("-v", help="Add this argument if you want even more info about the movie.", action="store_true")
+    parser.add_argument("title", help="Movie title you want to search for.", type=str, nargs='*')
+
+    args = parser.parse_args()
+
+    if len(args.title) > 1:
+        args.title = "".join([word+"+" for word in args.title])
+
+    m = omdb.Movie(args.title)
+
+    if m.results:
+        if args.v:
+            results = """{} ({})\n{}\nDirector: {}\nActors: {}\nRuntime: {}\nRating: {}"""
+            results = results.format(m.title,m.year,m.plot,m.director,m.actors,m.runtime,m.rating)
+            print(results)
+
+        else:
+            print("{} ({})\n{}\nRating: {}".format(m.title,m.year,m.plot,m.rating))
+    else:
+        print("No movie found.")
---
+++ omdb.py
@@ -0,0 +1,44 @@
+"""
+omdb-API wrapper.
+
+"""
+
+import requests
+import json
+
+class Movie():
+
+    def __init__(self, string):
+        self.get_data(string)
+
+    def get_data(self, string):
+
+        """Fetch json-data."""
+
+        get = requests.get("http://www.omdbapi.com/?t={}&y=&plot=short&r=json".format(string))
+        json_data = json.loads(get.text)
+
+        if json_data["Response"] == "True":
+            self.results = True
+            self.title = json_data["Title"]
+            self.year = json_data["Year"]
+            self.rating = json_data["imdbRating"]
+            self.imdbvotes = json_data["imdbVotes"]
+            self.plot = json_data["Plot"]
+            self.runtime = json_data["Runtime"]
+            self.director = json_data["Director"]
+            self.writer = json_data["Writer"]
+            self.actors = json_data["Actors"]
+            self.genre = json_data["Genre"]
+            self.country = json_data["Country"]
+            self.language = json_data["Language"]
+            self.poster = json_data["Poster"]
+        else:
+            self.results = False
+            pass
+
+
+
+if __name__ == '__main__':
+
+    pass
